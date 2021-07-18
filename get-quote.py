21f800d5a717823565f4e14f5faa38ee9af6aff2
starfowl <starfowl.shift@gmail.com>
2021-07-18 02:34:17 +0200

    "Adjusted my code to now be able to add quotes programmatically, print more than one quote at a time and easily add more. Officialy finished and well done."

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,37 @@
 import random
+import sys
+
+
 def primary():
-  print("Keep it logically awesome.")
+    # print('keep it logically awesome')
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+    f = open("quotes.txt")
+    quotes = [line.strip() for line in f.readlines()]
+    f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+    last = len(quotes) - 1
+    rnd = random.randint(0, last)
+    rnd0 = random.randint(0, last)
+    print(quotes[rnd])
+    print(quotes[rnd0])
 
-if __name__== "__main__":
-  primary()
+
+def addlines():
+    str0 = str(input("Quote you want to add to file: "))
+    f = open("quotes.txt", "a+")
+    f.write(str0 + "\n")
+    f.close()
+
+def removeblank():
+    output=""
+    with open("quotes.txt") as d:
+        for line in d:
+            if not line.isspace():
+                output+=line
+    f = open("quotes.txt", "w")
+    f.write(output)
+
+if __name__ == "__main__":
+    addlines()
+    removeblank()
+    primary()
